876b8bb9ed64eb481659bd892fec0011116ddc0d
KJTempas <fw6927mm@go.minneapolis.edu>
2020-03-31 15:42:35 -0500

    studenbt sign in app with API

---
+++ models/student.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+
+    let Student = sequelize.define('Student', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false//null fields are not allowed
+        }, starID: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true//this value must be iunique
+        }, present: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        }
+    })
+//force: false means tables are not deleted and recreated every time app restarts
+    Student.sync({force: false}).then( () => {
+        console.log('synced student table')
+    })
+    return Student
+}
